754dc9cbba7a6b2cb2a2cd87e587c32a0f22f3cd
Jeff Thorne <jeff.thorne@gmail.com>
2017-07-22 10:04:56 -0400

    updated ansible playbook

--- docker-flask-app/app.py
+++ docker-flask-app/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World! Dockerized"
+    return "Hello World! Dockerized 2"
 
 if __name__ == "__main__":
     app.run(debug=True,host='0.0.0.0', port=5001)
