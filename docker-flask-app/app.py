f8c37df768493a6e861b6fe0606f3fd2b23c02bb
Jeff Thorne <jeff.thorne@gmail.com>
2017-07-22 11:10:51 -0400

    updated ansible playbook

--- docker-flask-app/app.py
+++ docker-flask-app/app.py
@@ -1,9 +1,44 @@
-from flask import Flask
-app = Flask(__name__)
+import os
+from flask import Flask, render_template, request, flash, redirect
+from flask_assets import Environment
+from webassets import loaders
+
+app = Flask(__name__, static_folder='static', static_url_path='/static')
+app.config['DEBUG'] = True
+app.config['TEMPLATES_AUTO_RELOAD'] = True
+app.config['SECRET_KEY'] = "123"
+
+# asset pipeline
+assets = Environment(app)
+bundles = loaders.YAMLLoader('./static/js/js-assets.yml').load_bundles()
+[assets.register(name, bundle) for name, bundle in bundles.items()]
+bundles = loaders.YAMLLoader('./static/styles/css-assets.yml').load_bundles()
+[assets.register(name, bundle) for name, bundle in bundles.items()]
+
 
 @app.route("/")
 def hello():
-    return "Hello World! Dockerized 2"
+    return render_template('index.html', file_name="")
+
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_file():
+    print("in upload")
+    if request.method == 'POST':
+        print("In post")
+        # check if the post request has the file part
+        print(request)
+        if 'file' not in request.files:
+            flash('No file part')
+            return render_template('index.html', file_name="")
+
+        file = request.files['file']
+        print("filename: ", file.filename)
+        # if user does not select file, browser also
+        # submit a empty part without filename
+        file.save(os.path.join('/Users/jeff/app/', file.filename))
+
+        return render_template('index.html', file_name="Thanks for submitting %s. It looks tasty. --colin" % file.filename)
 
 if __name__ == "__main__":
     app.run(debug=True,host='0.0.0.0', port=5001)
