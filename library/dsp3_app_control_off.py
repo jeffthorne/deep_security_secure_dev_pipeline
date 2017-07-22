c0f6ffb4492ebe498162ff9354eed0a9e3aca8f9
Jeff Thorne <jeff.thorne@gmail.com>
2017-07-22 18:31:07 -0400

    added ansible module to turn DS app controll on/off

---
+++ library/dsp3_app_control_off.py
@@ -0,0 +1,28 @@
+#!/Users/jeff/playbooks/se_jam/venv/bin/python3.6
+
+from ansible.module_utils.basic import *
+
+import datetime
+import json
+from ansible.module_utils.basic import *
+
+
+from dsp3.models.manager import Manager
+
+
+module = AnsibleModule(argument_spec=dict(ds_tenant=dict(required=True, aliases=['ds_tenant']),
+                                          ds_user=dict(required=True, aliases=['ds_user']),
+                                          ds_password=dict(required=True, aliases=['ds_password'])))
+ds_tenant = module.params.get('ds_tenant')
+ds_user= module.params.get('ds_user')
+ds_password = module.params.get('ds_password')
+
+dsm = Manager(username=ds_user, password=ds_password, tenant=ds_tenant)
+dsm.set_trusted_update_mode(603, 3, False)
+api_version = dsm.get_api_version()
+dsm.end_session()
+
+
+print(json.dumps({
+    "api_version" : api_version
+}))
